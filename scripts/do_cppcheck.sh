3bee07cae58bed7e120611645a1f5fc1a8659a71
Peter Buchegger <peter@pbuchegger.at>
2016-09-14 01:35:22 +0200

    some update

--- scripts/do_cppcheck.sh
+++ scripts/do_cppcheck.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 set -e
 
-cppcheck --enable=all --suppress=missingIncludeSystem --inconclusive --xml --xml-version=2 Einsatz Fahrzeug Sync com \
+cppcheck --enable=all --suppress=missingIncludeSystem --inconclusive Einsatz Fahrzeug Sync com \
 -ibuild \
 -icom/build \
 -icom/test/com_convert_test \
 -Icom/src \
--Iprotocol/cpp_out --inline-suppr 2> cppcheck-result.xml
+-Iprotocol/cpp_out --inline-suppr
